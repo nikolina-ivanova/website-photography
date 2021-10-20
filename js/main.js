6763289eef71007552afe4380d5159b741c58f11
Nikolina Ivanova <nikolina9805@abv.bg>
2021-10-20 13:51:26 +0300

    light/dark mode added

--- js/main.js
+++ js/main.js
@@ -18,9 +18,30 @@ function responsiveNav() {
     var navIcon = document.getElementById("nav-icon");
     if(navLinks.className === "nav-links"){
         navLinks.className = "responsive";
-        navIcon.src = "images/icon-close.png";
+        navIcon.src = "../images/icon-close.png";
     }else{
     navLinks.className = "nav-links";
-    navIcon.src = "images/icon-open.png";
+    navIcon.src = "../images/icon-open.png";
+    }
+}
+
+//change light and dark theme
+function changeTheme() {
+    const toggle = document.getElementById("toggle");
+    if(toggle.className == "circle light-theme"){
+        // dark mode
+        toggle.classList.add("dark-theme");
+        toggle.classList.remove("light-theme");
+        toggle.src = "../images/icons/sun.png";
+        document.body.classList.add("dark-mode");
+        document.body.classList.remove("light-mode");
+    }
+    else {
+        // light mode
+        toggle.classList.remove("dark-theme");
+        toggle.classList.add("light-theme");
+        toggle.src = "../images/icons/moon.png";
+        document.body.classList.add("light-mode");
+        document.body.classList.remove("dark-mode");
     }
 }
--- js/signup.js
+++ js/signup.js
@@ -14,6 +14,8 @@ function signUpFunction(){
             this.password = password 
         }
     }
+    // minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter and 1 number
+    const reg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
     // check if the form if filled
     if(myname.value == ""){
@@ -26,7 +28,7 @@ function signUpFunction(){
     }
     else email.style.border = "none"
 
-    if(password.value == "" || password.value.length<8){
+    if(password.value == "" || reg.test(password.value) == false){
         password.style.border = "2px solid #67755e"
         passMesssage.innerText = "Must have at least 8 characters: \n 1 digit (0 - 9) \n 1 uppercase and 1 lowercase letter"
     }
@@ -35,8 +37,6 @@ function signUpFunction(){
         passMesssage.innerText = ""
     }
 
-    // minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter and 1 number
-    const reg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
     // add data to person and display it
     if(myname.value != "" & reg.test(password.value) & email.value != "") {
         const newperson = new Person(myname.value, email.value, password.value)
@@ -46,6 +46,8 @@ function signUpFunction(){
     else {
         message.innerText = "Please Fill the Form!"
     }
+
+    console.log(reg.test(password.value))
 }
 
 
